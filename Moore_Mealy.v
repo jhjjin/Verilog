7e43488e3d3cd4c8c30088a6aba719e1e07abd5f
Hyukjin Jeong <163715753+jhjjin@users.noreply.github.com>
2025-09-26 11:07:43 -0700

    Update Moore_Mealy.v

--- Moore_Mealy.v
+++ Moore_Mealy.v
@@ -88,3 +88,51 @@ module edge_detect_mealy (input wire clk, reset, level,output reg tick);
         endcase
 end
 endmodule
+
+
+
+// Simulation
+`timescale 1ns / 1ps
+
+module sim_topModule;
+    reg clk, reset, level;
+    wire tick_moore, tick_mealy;
+    topModule uut(clk, reset, level,tick_moore, tick_mealy);
+    
+    always begin
+    #5 clk =~clk;
+    
+    end 
+    
+    initial begin
+    clk=0; reset=0; level =0;
+
+    #5 reset =1;
+    #5 reset = 0; 
+ 
+   
+    #10 level = 0;
+    #10 level = 1;
+     #5 reset =1;
+    #5 reset = 0; 
+    #10 level = 0;
+    #10 level = 1;
+   
+    #10 level = 0;
+    #10 level = 1;
+    #10 level = 0;
+    #10 level = 1;
+    #10 level = 0;
+    
+    #5 reset =1;
+    #5 reset = 0; 
+    #10 level = 1;
+    #10 level = 0;
+    #10 level = 1;
+    #10 level = 0;
+    #10 level = 1;
+    #10 level = 0;
+    #10 level = 1;
+ 
+ end
+endmodule
